b22cecafda4348686b942a9bb51bdc93b677e665
Pawel Borkar <pawel@duck.com>
2026-07-01 15:32:51 +0530

    chore: use postgresql instead of shelve

--- app/api/v1/link.py
+++ app/api/v1/link.py
@@ -1,31 +1,38 @@
 import secrets
-import shelve
-from fastapi import APIRouter, HTTPException, status
+from typing import Annotated
+from fastapi import APIRouter, Depends, HTTPException, status
 from fastapi.responses import RedirectResponse
+from sqlalchemy.engine import url
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+from app.core.database import async_get_db
 from app.schemas.link import LinkCreate
+from app.models.url import URL
 
 
 router = APIRouter()
-# NOTE: This is for just for the very minimal setup to quickly get up and running ofcourse I'm gonna use environment variables in production ;)
-DB_NAME = "cutlet.db"
 
-
-#TODO: Add error handling
 """
 @params: Destination URL
 @method: POST
 @return: Short Code
 """
-@router.post("/links", tags=["Links"])
-async def create_short_code(link: LinkCreate):
-    short_code = secrets.token_urlsafe(6)
-    # short_code = base64.urlsafe_b64encode(key.encode('utf-8')).decode('utf-8').rstrip('=')
-    with shelve.open(filename=DB_NAME) as db:
-        db[short_code[:6]] = link.url
-        db.close()
 
-    print("SC:", short_code[:6])
-    return {"short_code": short_code[:6]}
+
+@router.post("/links", tags=["Links"])
+async def create_short_code(
+    link: LinkCreate, db: Annotated[AsyncSession, Depends(async_get_db)]
+):
+    try:
+        short_code = secrets.token_urlsafe(6)
+        new_url = URL(short_code=short_code, url=link)
+        db.add(new_url)
+        await db.flush()
+        return new_url
+
+    except Exception as e:
+        raise HTTPException(status_code=HTTP_400_BAD_REQUEST, detail=str(e))
+    # short_code = base64.urlsafe_b64encode(key.encode('utf-8')).decode('utf-8').rstrip('=')
 
 
 """
@@ -33,16 +40,8 @@ async def create_short_code(link: LinkCreate):
 @method: GET
 @return: Redirects to the Destination URL - uses 302 Temporary redirect
 """
+
+
 @router.get("/{short_code}")
 async def redirect_to_destination(short_code: str):
-    with shelve.open(DB_NAME) as db:
-        if short_code not in db:
-            raise HTTPException(
-                status_code=status.HTTP_404_NOT_FOUND,
-                headers=None,
-                detail="URL not found.",
-            )
-
-        return RedirectResponse(
-            url=db[short_code], status_code=302, headers=None, background=None
-        )
+    pass
