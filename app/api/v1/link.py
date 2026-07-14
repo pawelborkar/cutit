1384ac2f7549dcad1713d720061a7de866d12542
Pawel Borkar <pawel@duck.com>
2026-07-14 11:36:02 +0530

    feat: add url-redirect and counter
    now users can redirect to the destination url using the short url

--- app/api/v1/link.py
+++ app/api/v1/link.py
@@ -1,15 +1,23 @@
+from functools import lru_cache
 import secrets
 from fastapi import APIRouter, Depends, HTTPException
 from fastapi.responses import RedirectResponse
-from sqlalchemy.engine import url
+
+from sqlalchemy import select, update
 from sqlalchemy.ext.asyncio import AsyncSession
-from starlette.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
+
+from typing import Annotated
+from starlette.status import HTTP_400_BAD_REQUEST, HTTP_404_NOT_FOUND
+
+from app.core.config import settings
 from app.core.database import async_get_db
+
 from app.schemas.link import LinkCreate
 from app.models.url import URL
 
 
 router = APIRouter()
+
 
 """
 @params: Destination URL
@@ -41,5 +49,29 @@ async def create_short_code(
 
 
 @router.get("/{short_code}")
-async def redirect_to_destination(short_code: str):
-    pass
+@lru_cache(maxsize=500)
+async def redirect_to_destination(
+    short_code: str, db: Annotated[AsyncSession, Depends(async_get_db)]
+):
+    results = await db.execute(select(URL).where(URL.short_code == short_code))
+    url_entry = results.scalar_one_or_none()
+
+    if url_entry is None:
+        raise HTTPException(
+            status_code=HTTP_404_NOT_FOUND, detail="Short URL not found"
+        )
+
+    await db.execute(
+        update(URL).where(URL.short_code == short_code).values(count=URL.count + 1)
+    )
+    await db.commit()
+    return RedirectResponse(
+        url=url_entry.url, status_code=settings.app.APP_DEFAULT_REDIRECT_TYPE
+    )
+
+    # try:
+    #     _url = await db.get(entity=url, ident=short_code)
+    #     print(_url)
+
+    # except Exception as e:
+    #     raise HTTPException(status_code=HTTP_404_NOT_FOUND, detail=str(e))
