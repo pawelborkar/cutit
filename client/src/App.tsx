4559a96506caf73651c0938487a5e912130c6abf
Pawel Borkar <pawel@duck.com>
2026-07-27 01:16:24 +0530

    feat: add toast and tanstack query clients

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,12 +1,18 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import Dashboard from "./components/Dashboard/Dashboard";
+import { Toast } from "@heroui/react";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <>
-    <div className='bg-cyan-100'>
-Hello world 
-</div>
-    </>
-  )
+    <QueryClientProvider client={queryClient}>
+      <Toast.Provider />
+      <main className="flex bg-radial from-pink-400 from-40% to-fuchsia-700 w-[100vw] h-[100vh]">
+        <Dashboard />
+      </main>
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
